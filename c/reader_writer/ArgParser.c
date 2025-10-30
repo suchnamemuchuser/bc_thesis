e3c057e319d6ba030f956011dcfd0773cd7d6efd
Josef Michal <suchname@DESKTOP-KT0NG2O.localdomain>
2025-10-30 11:29:47 +0100

    move changes to laptop

--- c/reader_writer/ArgParser.c
+++ c/reader_writer/ArgParser.c
@@ -1,26 +1,50 @@
+#include <string.h>
+
 #define SOURCE_ZEROS 1
 #define SOURCE_GEN 2
 #define SOURCE_FILE 3
 
 #define READER_MAX_CAP 128
-#define BUFFER_DEF_SIZ 512000000 // 512 MB
+#define BUFFER_SIZE_DEF 512000000 // 512 MB
+#define DATA_RATE_DEF 3*1024 // 3MB/s
 
 typedef struct {
     // data creation rate
     int dataRate;
     // data source
     int dataSource;
+    // data destination
+    int dataDestination;
     // data destination - ipv4 or dname
-    char address[256];
+    char destName[256];
     // port
     int port;
 
 
-    // Writer reads zeroes, from file or from network
+    // Need to know data input and output:
+    // Input:
+    //      1. Network
+    //      2. File
+    //      3. Zeros
+    //
+    // Output:
+    //      1. Network
+    //      2. File
+
+
+    // Writer reads zeroes, from file, or from network
     // reader writes into file or network
 } argParser;
 
-int parseArguments(int argc, char* argv[], argParser* args)
+argParser parseArguments(int argc, char* argv[])
 {
-    
+    argParser args = { .dataRate = DATA_RATE_DEF, .dataSource = SOURCE_ZEROS};
+
+    for (int i = 0 ; i < argc ; i++)
+    {
+        if (strcmp(argv[i], "-i"))
+        {
+            printf("Got -i arg\n");
+        }
+    }
 }
--- c/telescope_side/main.c
+++
@@ -1,7 +0,0 @@
-// No complicated synchronization
-// Writer writes whenever he can
-// Reader should probably always read as much as he can
-// Can define a low limit for reader
-// If avaiable data is less than low limit, reader sets a bool, leaves the critical section, waits some time
-// Else reader copies data, sends over network
-// No semaphores or locks except
