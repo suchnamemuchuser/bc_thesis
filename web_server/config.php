4bb7b84851c919589ea07035c28d47d6d378044e
Josef Michal <pepin.michal@gmail.com>
2026-02-25 17:45:38 +0100

    Update: php config to absolute path

--- web_server/config.php
+++ web_server/config.php
@@ -1,9 +1,9 @@
 <?php
 
 // python executable for python scripts, from system or venv
-$python = '../../bc_thesis_web/.venv/bin/python3';
+$python = '/var/www/html/bc_thesis_web/.venv/bin/python3';
 
-$gen_obs_json_py = 'gen_obs_json.py';
+$gen_obs_json_py = '/var/www/bin/gen_obs_json.py';
 
 $process_log = '/var/lib/rt2/process_log.txt';
 
