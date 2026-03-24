003810e9093e9f3bff66b0203544a3664a949d8b
Josef Michal <pepin.michal@gmail.com>
2026-03-24 14:42:25 +0100

    Remove: python executable from config

--- web_server/config.php
+++ web_server/config.php
@@ -1,8 +1,5 @@
 <?php
 
-// python executable for python scripts, from system or venv
-$python = '/var/www/html/bc_thesis_web/.venv/bin/python3';
-
 $gen_obs_json_py = '/var/www/bin/gen_obs_json.py';
 
 $process_log = '/var/lib/rt2/process_log.txt';
