3819dfeb0bd875ad4b29b48be151ff240f410168
Josef Michal <pepin.michal@gmail.com>
2026-05-01 16:38:25 +0200

    Add: moved header to own php file

---
+++ web_server/header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>Pulsar Observation Planner</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Planner</a></li>
+            <li><a href="history.php">Planned Recordings</a></li>
+        </ul>
+    </nav>
+</header>
