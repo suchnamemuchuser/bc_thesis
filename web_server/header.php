2076ab5920579c33b0f9c67871b00a1c2c1351e7
Josef Michal <pepin.michal@gmail.com>
2026-05-02 19:15:10 +0200

    Add: header title set at include

--- web_server/header.php
+++ web_server/header.php
@@ -1,9 +1,9 @@
 <header>
-    <h1>Pulsar Observation Planner</h1>
+    <h1><?php echo isset($pageTitle) ? $pageTitle : "Radio telescope management" ?></h1>
     <nav>
         <ul>
             <li><a href="index.php">Planner</a></li>
-            <li><a href="history.php">Planned Recordings</a></li>
+            <li><a href="history.php">Observations</a></li>
         </ul>
     </nav>
 </header>
